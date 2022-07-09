040a5390ada64d33a8e268f502dd3faf54abd51e
MymiRC <stephania@family.com.mx>
2022-07-09 00:45:01 -0700

    Haptics & Typography

--- Assets/Scripts/Saber.cs
+++ Assets/Scripts/Saber.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-
 public class Saber : MonoBehaviour
 {
-    [SerializeField] string colorLayer;
-
+    public XRBaseController controller;
     XRRayInteractor rayInteractor;
     ScoreManager scoreManager;
+    public float defaultamplitude = 0.2f;
+    public float defaultduration = 0.5f;
 
     void Start()
     {
@@ -22,16 +22,17 @@ public class Saber : MonoBehaviour
             raycastHit.collider.TryGetComponent<Object>(out var hitObject);
             if (hitObject != null)
             {
-                if (hitObject.gameObject.layer == LayerMask.NameToLayer(colorLayer))
-                {
+                //if (hitObject.gameObject.layer == LayerMask.NameToLayer(colorLayer))
+                //{
                     if (!hitObject.hit)
                     {
                         Debug.Log(hitObject.gameObject.layer);
+                        controller.SendHapticImpulse(defaultamplitude,defaultduration);
                         hitObject.DestroyObject();
                         scoreManager.AddScore();
                     }
                         
-                }
+                //}
             }
         }
     }
